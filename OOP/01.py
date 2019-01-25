e84a402e6a59e0c13acf3f342708d0de5ba09653
guochunhua <guochunhua@guinong.com>
2019-01-25 13:45:57 +0800

    类方法的访问

--- OOP/01.py
+++ OOP/01.py
@@ -121,7 +121,7 @@ class StudentA():
     name = "chunhua"
     age = 18
 
-    def my_info(self):
+    def myInfo(self):
         self.name = "huahua"
         self.age = 200
         print("My name is {0}".format(self.name))
@@ -130,7 +130,7 @@ class StudentA():
 my = StudentA()
 # 对象my把自己作为参数传入函数my_info()中
 # 即self.name 等价于 my.name ; self.age 等价于 my.age
-my.my_info()
+my.myInfo()
 
 
 # 有self 与 没有self的区别
@@ -140,20 +140,108 @@ class Teacher():
     age = 20
 
     # 定义一个非绑定类的方法
-    def he_self(self):
+    def heSelf(self):
         self.name = "liudana"
         self.age = 30
         print("My name is {0}".format(self.name))
         print("My age is {0}".format(self.age))
     # 定义一个绑定类的方法
-    def he_he():
+    def heHe():
         print("他说他是python老司机...")
 
 ta = Teacher()
 # 调用非绑定类函数使用对象名
 # 对象ta会作为参数传入函数中
-ta.he_self()
+ta.heSelf()
 
 # 调用绑定类函数使用类名
 # 此时类名不会作为参数传入函数中
-Teacher.he_he()
+Teacher.heHe()
+
+
+# 使用类 访问绑定类方法，且方法中需要访问类中的成员
+print("使用类 访问绑定类方法，且方法中需要访问类中的成员，使用 __class__.方法的示例------------------------------")
+
+class MyInfo():
+    name = "过去"
+    age = 18
+
+    def nowSelf(self):
+        self.name = "现在"
+        self.age = 30
+        print("我{0}很年轻{1}，做错很多事，很不开心！！！".format(__class__.name, __class__.age))
+        print("我{0}年纪大了，已经{1}，但很开心。。。".format(self.name, self.age))
+
+    def nowBefor():
+        print("{0}的都已经成为历史，{0}的算了。。。".format(__class__.name))
+        print("其实我还很年轻，我现在心态是{0}岁。。。".format(__class__.age))
+
+wo = MyInfo()
+wo.nowSelf()
+# 访问绑定类的方法时，使用对象名.访问时出错。
+# wo.nowBefor()
+# 访问绑定类方法时 需使用类名.访问。
+
+MyInfo.nowBefor()
+
+
+
+# self 案例
+class B():
+    name = "学生"
+    age = 18
+
+    # 构造函数
+    def __init__(self):
+        self.name = "程序员"
+        self.age = 30
+
+    def say(self):
+        print(self.name)
+        print(self.age)
+
+class C():
+    name = "C++"
+    age = 35
+
+# 实例化
+a = B()
+# 此时，a为对象实例 访问非绑定类的方法时，系统会默认把a作为第一参数传入say函数
+a.say()
+
+# 此时，B为类实例，访问非绑定类的方法时，需要手动将对象a作为参数传入say函数，即self被a替换
+B.say(a)
+
+# 此时，把B作为参数传入say函数，即self被B替换了，即调用B类中的 name 和 age 成员（前提条件时，B类中必须存在name和age属性成员，否则报错）
+B.say(B)
+# 同理，可以把类C 作为参数传入say函数，同样C类中也必须存在name和age属性成员，否则报错
+B.say(C)
+
+# 以上代码 利用了鸭子模型（看起来像鸭子、具有鸭子的属性<有翅膀、会游泳、嘎嘎嘎叫...>，就认为你是鸭子）
+# 同理以上代码中使用的 B类和 C类 不管是否为同一类型，只要存在函数say中所需要的属性<name, age>,即认为B和C是同一类
+
+
+
+# 私有变量示例
+
+class Person():
+    # name 是公有的成员
+    name = "chunhua"
+    # __age 就是私有成员，在age前面添加两个下划线即可
+    __age = 18
+
+p = Person()
+# name 作为公有变量，在外部访问，没毛病...
+print(p.name)
+
+# __age 做为私有变量，此时在外部方式，则报错。。。'object has no attribute'
+# print(p.__age)
+# name mangling 技术
+# 查看类内所有成员
+print(Person.__dict__)
+p._Person__age = 180
+print(p._Person__age)
+print(Person.__dict__)
+
+
+
